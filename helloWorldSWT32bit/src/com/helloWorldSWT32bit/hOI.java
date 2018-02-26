bedc17c2b8a055cb7356c36173711fc6e272cad6
Hate Love <gitREKT@skrub.hitmark>
2018-02-26 11:22:35 +0300

    Added comments to 32bitSWT project

--- helloWorldSWT32bit/src/com/helloWorldSWT32bit/hOI.java
+++ helloWorldSWT32bit/src/com/helloWorldSWT32bit/hOI.java
@@ -13,25 +13,31 @@ import org.eclipse.swt.widgets.Text;
 public class hOI {
     public static void main(String[] argv)
     {
+        //all SWT capabilities are here
         Display display = new Display();
 
+        //window (that's it)
         Shell shell = new Shell(display);
 
         shell.setLayout(new FillLayout());
 
+        //label (literally just text on window, that's it)
         Label label = new Label(shell, SWT.BORDER);
         label.setText("hOIIIIII! I'm TEMMIE!");
         label.setToolTipText("And dis is my friend: TEMMIE!!");
 
+        //text widget
         Text text = new Text(shell, SWT.NONE);
         text.setText("Hi. My name is Bob.");
         text.setBackground(display.getSystemColor(SWT.COLOR_BLACK));
         text.setForeground(display.getSystemColor(SWT.COLOR_WHITE));
 
-
+        //was made to fit widgets to preferable sizes
+        //in real life does NOTHING (just like me, lol)
         text.pack();
         label.pack();
-        
+
+        //making window and making it work till the close
         shell.open();
         while(!shell.isDisposed())
         {
@@ -41,6 +47,7 @@ public class hOI {
             }
         }
 
+        //disposal tool
         display.dispose();
     }
 }
